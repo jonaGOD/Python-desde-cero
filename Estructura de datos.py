d38d9487c8c11f34f14ef9b7ec8aaee33c8acfeb
Jonathan <jonarodriguez91@gmail.com>
2025-10-22 11:02:04 -0300

    Estructura

---
+++ Estructura de datos.py
@@ -0,0 +1,29 @@
+#---------------------------------------------------
+# TIPOS DE DATOS SECUENCIA / COLECCION
+#---------------------------------------------------
+
+lista = [1,2,3,4,5] # Siempre entre [], es ordenada y mutable.
+tupla = (1,2,3,4,5) # Ordenada pero inmutable.
+rango = range (0,10) # Secuencia inmutable de numeros.
+
+#---------------------------------------------------
+# TIPOS DE DATOS DE MAPEO
+#---------------------------------------------------
+
+diccionario = { # Coleccion no ordenada de pares clave-valor
+    "nombre": "Jonathan",
+    "apellido": "Rodriguez",
+    "edad": 34
+}
+
+#---------------------------------------------------
+# TIPOS DE DATOS CONJUNTOS
+#---------------------------------------------------
+
+conjunto = {1,2,3,4,5} # Coleccion no ordenada y mutable de elementos únicos.
+
+#---------------------------------------------------
+# TIPOS DE DATOS NULO
+#---------------------------------------------------
+
+nulo = None  # Representa la ausencia de valor o la no definición.
